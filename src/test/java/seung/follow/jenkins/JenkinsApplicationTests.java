cb4d764f2ef90f929b7635b4f2f2c712ccb6c3c6
chloe_choi <plumxeun@gmail.com>
2024-08-23 10:12:32 +0900

    Initial Commit

---
+++ src/test/java/seung/follow/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package seung.follow.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
